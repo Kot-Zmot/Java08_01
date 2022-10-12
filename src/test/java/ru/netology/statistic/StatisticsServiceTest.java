2042364fe4d9d591a8133f7f818a5ec6da806209
Kot-Zmot <meledin.av52@gmail.com>
2022-10-12 20:28:02 +0300

    100% test coverage

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,17 @@ public class StatisticsServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxInOtherUnits() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {4, 5, 8, 12, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
 }
